dfc960d0242ef60afb88b44c4836f4bc6944403a
Alessandro Morandi <alessandro@headshift.com>
2011-01-04 12:13:29 +0000

    Minor code reorganization.

--- lib/headshift/profile_formatter.rb
+++ lib/headshift/profile_formatter.rb
@@ -10,12 +10,29 @@ module Headshift
       @failures = []
     end
     
-    def before_step(*args)
-      @timestamp = Time.now
-    end
-    
     def before_features(*args)
       @start_time = Time.now
+    end
+    
+    def after_features(*args)
+      puts
+      puts "======================="
+      puts "#{@steps.count} steps and #{@examples.count} examples executed in #{Time.at(Time.now - @start_time).gmtime.strftime("%Hh %Mm %Ss")}"
+      puts
+      print_formatted_hash(@steps, "Steps")
+      puts
+      print_formatted_hash(@examples, "Examples")
+      unless @failures.empty?
+        puts "======================="
+        puts "Failing Scenarios:"
+        @failures.each do |failure|
+          puts "cucumber #{failure}"
+        end
+      end
+    end
+    
+    def before_step(*args)
+      @timestamp = Time.now
     end
     
     def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
@@ -36,27 +53,6 @@ module Headshift
       end
       
       super
-    end
-    
-    def after_features(*args)
-      puts
-      puts "======================="
-      puts "#{@steps.count} steps and #{@examples.count} examples executed in #{Time.at(Time.now - @start_time).gmtime.strftime("%Hh %Mm %Ss")}"
-      puts
-      print_formatted_hash(@steps, "Steps")
-      puts
-      print_formatted_hash(@examples, "Examples")
-      unless @failures.empty?
-        puts "======================="
-        puts "Failing Scenarios:"
-        @failures.each do |failure|
-          puts "cucumber #{failure}"
-        end
-      end
-    end
-    
-    def scenario_name(keyword, name, file_colon_line, source_indent)
-      @scenario_file_colon_line = file_colon_line
     end
     
     def before_examples(examples)
@@ -95,22 +91,27 @@ module Headshift
       end
     end
     
-    def print_formatted_hash(hash, title, sort_by=:tot_time, limit=20)
-      key_width = title.length
-      count_width = avg_time_width = tot_time_width = 0
-      hash.each do |key, value|
-        key_width = [key_width, key.length].max
-        count_width = [count_width, value[:count].to_s.length].max
-        avg_time_width = [avg_time_width, ("%.6f" % value[:avg_time]).length].max
-        tot_time_width = [tot_time_width, ("%.6f" % value[:tot_time]).length].max
-      end
-      
-      sorted_hash = hash.sort {|x,y| x[1][sort_by] <=> y[1][sort_by]}
-      
-      puts "| #{title.ljust(key_width)} | #{"#".ljust(count_width)} | #{"Avg Time".ljust(avg_time_width)} | #{"Tot Time".ljust(tot_time_width)} |"
-      sorted_hash.reverse[0..limit].each do |step|
-        puts "| #{step[0].ljust(key_width)} | #{step[1][:count].to_s.ljust(count_width)} | #{("%.6f" % step[1][:avg_time]).ljust(avg_time_width)} | #{("%.6f" % step[1][:tot_time]).ljust(tot_time_width)} |"
-      end
+    def scenario_name(keyword, name, file_colon_line, source_indent)
+      @scenario_file_colon_line = file_colon_line
     end
+    
+    private
+      def print_formatted_hash(hash, title, sort_by=:tot_time, limit=20)
+        key_width = title.length
+        count_width = avg_time_width = tot_time_width = 0
+        hash.each do |key, value|
+          key_width = [key_width, key.length].max
+          count_width = [count_width, value[:count].to_s.length].max
+          avg_time_width = [avg_time_width, ("%.6f" % value[:avg_time]).length].max
+          tot_time_width = [tot_time_width, ("%.6f" % value[:tot_time]).length].max
+        end
+      
+        sorted_hash = hash.sort {|x,y| x[1][sort_by] <=> y[1][sort_by]}
+      
+        puts "| #{title.ljust(key_width)} | #{"#".ljust(count_width)} | #{"Avg Time".ljust(avg_time_width)} | #{"Tot Time".ljust(tot_time_width)} |"
+        sorted_hash.reverse[0..limit].each do |step|
+          puts "| #{step[0].ljust(key_width)} | #{step[1][:count].to_s.ljust(count_width)} | #{("%.6f" % step[1][:avg_time]).ljust(avg_time_width)} | #{("%.6f" % step[1][:tot_time]).ljust(tot_time_width)} |"
+        end
+      end
   end
 end
